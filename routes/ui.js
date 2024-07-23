b3ed914c9b383034073ca43e52dcd9a942d1976b
Kyle March-MacCuish <kylemacuish@gmail.com>
2024-07-23 03:26:18 -0230

    styled the css to bemore on theme

--- routes/ui.js
+++ routes/ui.js
@@ -11,4 +11,28 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/cartoons/new', (req, res) => {
+    res.render('new');
+});
+
+router.get('/cartoons/:id/edit', async (req, res) => {
+    try {
+        const cartoons = await dal.getCartoons();
+        const cartoon = cartoons.find(c => c.id == req.params.id);
+        res.render('edit', { cartoon });
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+router.get('/cartoons/:id', async (req, res) => {
+    try {
+        const cartoons = await dal.getCartoons();
+        const cartoon = cartoons.find(c => c.id == req.params.id);
+        res.render('show', { cartoon });
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
 module.exports = router;
